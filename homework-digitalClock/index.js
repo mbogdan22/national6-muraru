88e5265fabc3130f7b339054e9a872d9f7ff9a74
b22 <muraru_bogdan22@yahoo.com>
2021-03-06 13:47:59 +0200

    homeworkDone

--- homework-digitalClock/index.js
+++ homework-digitalClock/index.js
@@ -1,17 +1,21 @@
 console.log("JavaScript - Digital Clock");
 
-const secondsParagraphs = document.querySelectorAll(".seconds p");
-let seconds = 0;
+    const secondsParagraphs = document.querySelectorAll(".seconds p");
+        let seconds = 0;
 
-const minutesParagraphs = document.querySelectorAll(".minutes p");
-let minutes = 0;
+    const minutesParagraphs = document.querySelectorAll(".minutes p");
+        let minutes = 0;
 
-const hoursParagraphs = document.querySelectorAll(".hours p");
-let hours = 0; 
+    const hoursParagraphs = document.querySelectorAll(".hours p");
+        let hours = 0; 
+
+    let intervalId;
+
     
-    //document.getElementById("start").addEventListener();
+    document.getElementById("start").addEventListener("click", function (){
 
-    let timeStart = setInterval(function() {
+
+        intervalId = setInterval(function() {
     
         renderDigits(seconds, secondsParagraphs);
         renderDigits(minutes, minutesParagraphs);
@@ -30,7 +34,28 @@ let hours = 0;
             hours = 0;
         }
     
-     }, 1000);
+        }, 200);
+    });
+
+    document.getElementById("stop").addEventListener("click", function() {
+        clearInterval(intervalId);
+    });
+
+    document.getElementById("reset").addEventListener("click", function() {
+        seconds = 0;
+        minutes = 0;
+        hours = 0;
+
+        secondsParagraphs[0].innerText = 0;
+        secondsParagraphs[1].innerText = 0;
+        
+        minutesParagraphs[0].innerText = 0;
+        minutesParagraphs[1].innerText = 0;
+      
+        hoursParagraphs[0].innerText = 0;
+        hoursParagraphs[1].innerText = 0;
+        
+    });
 
 
     function renderDigits(nr, pList) {
@@ -43,5 +68,22 @@ let hours = 0;
         } else {
             pList[0].innerText = 0;
             pList[1].innerText = digitList[0];
-    }
-};
+        }
+    };
+
+
+    document.getElementById("save").addEventListener("click", function() {
+        const setLap = document.createElement("p");
+        document.body.appendChild(setLap);
+        var seeSeconds = seeDigits(seconds);
+        var seeMinutes = seeDigits(minutes);
+        var seeHours = seeDigits(hours);
+        setLap.innerText = `${seeHours}:${seeMinutes}:${seeSeconds}`
+    });
+
+    function seeDigits(digit) {
+        if (digit < 10) {
+            digit = `0${digit}`;
+        }
+        return digit;
+    };  
